994e69644fadcd5d8c400256b389dd1467d9a4e0
xx0lucas0xx <lucas.Rempart@gmail.com>
2023-03-24 11:47:52 -0600

    Add project files.

---
+++ WebAppMusicVideo/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+//Functions to be called onmouseover and onmouseout to change color of "Hmmm"
+function newColor() {
+    document.getElementById("Hmmm").style.color = "darkolivegreen";
+}
+
+function oldColor() {
+    document.getElementById("Hmmm").style.color = "darkred";
+}
